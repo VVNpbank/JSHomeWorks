837af89c2da303e1d11795a5646579b37e7319af
VVNpbank <40174761+VVNpbank@users.noreply.github.com>
2022-01-17 20:43:50 +0200

    Create HW1.js

---
+++ HW1.js
@@ -0,0 +1,11 @@
+//####Задача 1
+//Перепишите код, заменив оператор `if` на тернарный оператор `?`
+//```js
+//var result;
+//if (a + b < 4) {
+//result = true;
+//} else {
+//result = false;
+//}
+const a=1; b=2;
+console.log(result = (a + b < 4) ? true : false);
